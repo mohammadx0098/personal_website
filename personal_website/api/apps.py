560712f38cee8010e4da1daa7af8b08d024cbd34
mohammad <mohammadx0098@gmail.com>
2024-11-01 12:35:52 +0000

    init: init project with cookiecutter

---
+++ personal_website/api/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ApiConfig(AppConfig):
+    name = 'personal_website.api'
---
+++ personal_website/api/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path, include
+
+urlpatterns = [
+    # path('blog/', include(('personal_website.blog.urls', 'blog')))
+]
